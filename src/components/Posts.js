e4f77efe7a9cb5ebc2263fd5c32a24b4578b1670
drake021 <drake021@gmail.com>
2021-02-23 19:36:48 -0500

    Travis is pretty cool

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const Posts = (props) => {
-const posts = props.placeholder.posts;
-const title = props.placeholder.posts.title;
-const description = props.placeholder.posts.description;
-const price = props.placeholder.price;
-const seller = props.placeholder.seller;
-const location = props.placeholder.location;
 
+const BASE_URL = 'https://strangers-things.herokuapp.com/api/2004-UNF-HY-WEB-PT';
+
+
+async function fetchPosts() {
+  try { 
+     const response = await fetch(`${BASE_URL}/posts`)
+    const data = await response.json();
+  return console.log(data);
+} catch (error) {
+  throw error;
+}
+}
+fetchPosts();
+
+const Posts = (props) => {
+const title = props.title;
+const description = props.description;
+const price = props.price;
+const seller = props.seller;
+const location = props.location;
+
+console.log(props.title);
 
     return <main>
         <div>
@@ -20,47 +35,13 @@ const location = props.placeholder.location;
                 <h3>{title}</h3>
                 <p>{description}</p>
                 <span className="title">Price</span>
-                <span className="content">{price}</span>
+                <span className="content">{ price}</span>
                 <span className="title">Seller</span>
                 <span className="content">{seller}</span>
                 <span className="title">Location</span>
                 <span className="content">{location}</span>
                 <button>Send Message</button>
             </section>
-        </div>
-        <div>
-            <aside>
-                <header>
-                    <h1>Submit Post</h1>
-                </header>
-                <form>
-                    <label for="post-title">Post Title</label>
-                    <input
-                        id="post-title"
-                        type="text"
-                        placeholder="enter title..."
-                    />
-                    <label for="post-description">Post Description</label>
-                    <input
-                        id="post-description"
-                        type="text"
-                        placeholder="enter description..."
-                    />
-                    <label for="price">Price</label>
-                    <input
-                        id="price"
-                        type="text"
-                        placeholder="enter price..."
-                    />
-                    <label for="post-title">Details</label>
-                    <input
-                        id="details"
-                        type="text"
-                        placeholder="enter details..."
-                    />
-                </form>
-                <button>Submit</button>
-            </aside>
         </div>
     </main>
 
