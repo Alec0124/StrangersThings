76dbc1abb4d6d40805a1601e10fea618be4f568c
callarahne <callarjan@gmail.com>
2021-02-28 17:22:59 -0500

    more changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,8 @@
 import React, { useState, useRef } from 'react';
 import "./Auth.css";
-import { fetchLogin } from "../api/index.js";
+import { fetchLogin, fetchMe } from "../api/index.js";
 import { Redirect } from 'react-router-dom';
-// import { fetchLogin, fetchMe } from "../api/index.js";
-
 const BASE_URL = 'https://strangers-things.herokuapp.com/api/2010_UNF_RM_WEB_PT/users/login'
-
 
 const Login = ({ user, setUser }) => {
 
@@ -14,68 +11,6 @@ const Login = ({ user, setUser }) => {
   const [password, setPassword] = useState('');
   // const [confirmPassword, setConfirmPassword] = useState('');
   const [errors, setErrors] = useState(null);
-  // const [userList, setUserList] = useState([]);
-
-  // let username = document.getElementById('username');
-  // let password = document.getElementById('password');
-
-
-
-  // function store() {
-
-
-  // function check() {
-
-  //   const storedUsername = localStorage.getItem('username');
-  //   const storedPassword = localStorage.getItem('password');
-
-  //   const username = document.getElementById('username');
-  //   const password = document.getElementById('password');
-
-  //   if(username.value !== storedUsername || password.value !== storedPassword) {
-  //       alert('ERROR');
-  //   }else {
-  //       alert('You are loged in.');
-  //   }
-
-  // }
-
-
-
-
-  // function Login() {
-  //     useEffect(() => {
-  //         async function registerUser() {
-  //             const result = await fetch(
-  //                 'https://strangers-things.herokuapp.com/api/2010_UNF_RM_WEB_PT/users/login', {
-  //                     method: "POST",
-  //                     headers: {
-  //                       'Content-Type': 'application/json'
-  //                     },
-  //                     body: JSON.stringify({
-  //                       user: {
-  //                         username,
-  //                         password,
-  //                       }
-  //                     })
-  //                   }).then(response => response.json())
-  //                     .then(result => {
-  //                       console.log(result);
-  //                     })
-  //                     .catch(console.error);
-  //                 }
-  //     })
-
-  // useEffect(() => {
-  //   getUsers()
-  //     .then(users => {
-  //     //   setUserList(users)
-  //     // })
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // }, []);
 
   const usernameOnChange = (event) => {
     setUsername(event.target.value);
@@ -83,9 +18,6 @@ const Login = ({ user, setUser }) => {
   const passwordOnChange = (event) => {
     setPassword(event.target.value);
   }
-  // const confirmPasswordOnChange = (event) => {
-  //   setConfirmPassword(event.target.value);
-  // }
   const submitIsValid = () => {
     let result = { test: true };
     if (password.length < 4) {
@@ -98,12 +30,25 @@ const Login = ({ user, setUser }) => {
     }
     return result;
   }
-  async function submitOnClick(event) {
+  const updateUserState = (object, token) => {
+    console.log('updateUserState() running');
+    console.log('object', object);
+    console.log('token', token);
+    let result = { ...object };
+    result.token = token;
+    setRefUser(result);
+  }
+  const myStateRef = useRef(user);
+  const setRefUser = data => {
+    myStateRef.current = data;
+    setUser(data);  
+  };
+
+  const submitOnClick = (event) => {
     event.preventDefault();
     if (submitIsValid().test) {
       fetchLogin(username, password)
         .then((response) => {
-          console.log('response', response);
           // {
           //   "success": true,
           //   "error": null,
@@ -112,12 +57,15 @@ const Login = ({ user, setUser }) => {
           //     "message": "Thanks for logging in to our service."
           //   }
           // }
+
           if (response.success) {
-            setUser({
-              username: username,
-              token: response.data.token,
-            });
-            return <Redirect to='/posts' />;
+
+            fetchMe(response.data.token).then(data => {
+              updateUserState(data.data, response.data.token);
+              return data;
+            }).then(data => {
+              localStorage.setItem('user', JSON.stringify(myStateRef.current));
+            })
           } else {
             setErrors(
               <>
@@ -135,27 +83,26 @@ const Login = ({ user, setUser }) => {
         </>
       );
     }
+
+    console.log('aferFetchMeInLogin user: ', user);
   }
 
 
   return (
     <main id="main-holder">
-      <div id="main-content">
-        {user ? <Redirect to='/posts' /> : null}
-        {/* <div className='App'></div> */}
-        <h1 id="login-header">Login</h1>
-        <div id="login-error-msg-holder">
-          <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
-        </div>
-        <form id="login-storage">
-          <input onChange={usernameOnChange} type="text" name="username" id="username-field" className="login-storage-field" placeholder="Username"></input>
-          <input onChange={passwordOnChange} type="password" name="password" id="password-field" className="login-storage-field" placeholder="Password"></input>
-          <input onClick={submitOnClick} type="submit" value="Login" id="login-storgae-submit"></input>
-          {errors}
-        </form>
+      {user ? <Redirect to='/posts' /> : null}
+      <div className='App'></div>
+      <h1 id="login-header">Login</h1>
+      <div id="login-error-msg-holder">
+        <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
       </div>
+      <form id="login-storage">
+        <input onChange={usernameOnChange} type="text" name="username" id="username-field" className="login-storage-field" placeholder="Username"></input>
+        <input onChange={passwordOnChange} type="password" name="password" id="password-field" className="login-storage-field" placeholder="Password"></input>
+        <input onClick={submitOnClick} type="submit" value="Login" id="login-storgae-submit"></input>
+        {errors}
+      </form>
     </main>
   )
 }
-
 export default Login;
